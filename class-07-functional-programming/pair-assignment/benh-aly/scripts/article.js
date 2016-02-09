b3e573c0e7a71bb31085d8bef816a25bf8cb3703
bharding2 <b.harding@gmail.com>
2016-02-09 14:25:56 -0800

    edited fetchAll to take a callback as a parameter, compiled handlebars for admin page, iifed article.js, chained to get numwordsall, allauthors, and numwordsby author

---
+++ class-07-functional-programming/pair-assignment/benh-aly/scripts/article.js
@@ -0,0 +1,94 @@
+
+
+(function(module) {
+
+  function Article (opts) {
+    this.author = opts.author;
+    this.authorUrl = opts.authorUrl;
+    this.title = opts.title;
+    this.category = opts.category;
+    this.body = opts.body;
+    this.publishedOn = opts.publishedOn;
+  }
+
+  Article.all = [];
+
+  Article.prototype.toHtml = function() {
+    var template = Handlebars.compile($('#article-template').text());
+
+    this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+    this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+    this.body = marked(this.body);
+
+    return template(this);
+  };
+
+  Article.loadAll = function(rawData) {
+    rawData.sort(function(a,b) {
+      return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+    });
+
+    // DONE: Refactor this forEach code, by using a `.map` call instead, since want we are trying to accomplish
+    // is the transformation of one colleciton into another.
+    // rawData.forEach(function(ele) {
+    //   Article.all.push(new Article(ele));
+    // })
+    Article.all = rawData.map(function(ele) {
+      return new Article(ele);
+    });
+  };
+
+  Article.fetchAll = function(viewF) {
+    if (localStorage.rawData) {
+      Article.loadAll(JSON.parse(localStorage.rawData));
+      viewF();
+    } else {
+      $.getJSON('/data/hackerIpsum.json', function(rawData) {
+        Article.loadAll(rawData);
+        localStorage.rawData = JSON.stringify(rawData); // Cache the json, so we don't need to request it next time.
+        viewF();
+      });
+    }
+  };
+
+  Article.numWordsAll = function() {
+    return Article.all.map(function(article) {
+      return article.body.match(/\b\w+/g).length;
+    })
+    .reduce(function(a, b) {
+      return a + b;
+    })
+  };
+
+  Article.allAuthors = function() {
+    return Article.all.map(function(article) {
+      return article.author;
+    })
+    .reduce(function(a, b) {
+      if (a.indexOf(b) < 0) {
+        a.push(b);
+      }
+      return a;
+    },[]);
+  };
+
+  Article.numWordsByAuthor = function() {
+    return Article.allAuthors().map(function(author) {
+      return {
+                name: author,
+                numWords: Article.all.filter(function(article) {
+                  return article.author === author;
+                })
+                .map(function(article) {
+                  return article.body.match(/\b\w+/g).length;
+                })
+                .reduce(function(a, b) {
+                  return a + b;
+                })
+             }
+    })
+  };
+
+//expose Article to window
+module.Article = Article;
+}(window));
